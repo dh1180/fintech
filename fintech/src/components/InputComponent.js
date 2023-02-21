4baa340cf7dc204cebe7cac701f842a3214c9115
dh1180 <mjdh1936@gmail.com>
2023-02-21 15:13:12 +0900

    state를 활용한 데이터 변경 감지와 리렌더링

--- fintech/src/components/InputComponent.js
+++ fintech/src/components/InputComponent.js
@@ -1,16 +1,23 @@
 import {useState} from "react"
 
 const InputComponents = (props) => {
-  const value = "test"
+    // let value = "test"
     const [name, setName] = useState("test")
     const handleChangeInput = (event) => {
       console.log(event.target.value)
+      setName(event.target.value)
     }
+
+    const handleClickButton = () => {
+      alert(name)
+    }
+
+
     return (
       <>
       <p>{name}</p>
       <input onChange={handleChangeInput}></input>
-      <button>입력</button>
+      <button onClick={handleClickButton}>입력</button>
       </>
     )
 }
